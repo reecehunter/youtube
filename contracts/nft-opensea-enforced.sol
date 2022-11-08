4c5ee32f4d012c5b1f42229bbe06881759dc1a93
reecehunter <heecerunter@gmail.com>
2022-11-08 16:02:00 -0500

    Added OpenSea Enforcer NFT smart contract

---
+++ contracts/nft-opensea-enforced.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "contracts/DefaultOperatorFilterer.sol";
+
+contract NFT is ERC721, Ownable, DefaultOperatorFilterer {
+    using Strings for uint256;
+
+    uint public constant MAX_TOKENS = 10000;
+    uint private constant TOKENS_RESERVED = 5;
+    uint public price = 100000000000000000;
+    uint256 public constant MAX_MINT_PER_TX = 10;
+
+    bool public isSaleActive;
+    uint256 public totalSupply;
+    mapping(address => uint256) private mintedPerWallet;
+
+    string public baseUri;
+    string public baseExtesion = ".json";
+
+    constructor() ERC721("NFT Name", "SYMBOL") {
+        baseUri = "ipfs://xxxxxxxxxxxxxxxxxxxxxxxxxxxxx/";
+        for(uint256 i = 1; i <= TOKENS_RESERVED; ++i) {
+            _safeMint(msg.sender, i);
+        }
+        totalSupply = TOKENS_RESERVED;
+    }
+
+    // Public Functions
+    function mint(uint256 _numTokens) external payable {
+        require(isSaleActive, "The sale is paused.");
+        require(_numTokens <= MAX_MINT_PER_TX, "You cannot mint that many in one transaction.");
+        require(mintedPerWallet[msg.sender] + _numTokens <= MAX_MINT_PER_TX, "You cannot mint that many total.");
+        uint256 curTotalSupply = totalSupply;
+        require(curTotalSupply + _numTokens <= MAX_TOKENS, "Exceeds total supply.");
+        require(_numTokens * price <= msg.value, "Insufficient funds.");
+
+        for(uint256 i = 1; i <= _numTokens; ++i) {
+            _safeMint(msg.sender, curTotalSupply + i);
+        }
+        mintedPerWallet[msg.sender] += _numTokens;
+        totalSupply += _numTokens;
+    }
+
+    // Owner-only functions
+    function flipSaleState() external onlyOwner {
+        isSaleActive = !isSaleActive;
+    }
+
+    function setBaseUri(string memory _baseUri) external onlyOwner {
+        baseUri = _baseUri;
+    }
+
+    function setPrice(uint256 _price) external onlyOwner {
+        price = _price;
+    }
+
+    function withdrawAll() external payable onlyOwner {
+        uint256 balance = address(this).balance;
+        uint256 balanceOne = balance * 70 / 100;
+        uint256 balanceTwo = balance * 30 / 100;
+        ( bool transferOne, ) = payable(0x7ceB3cAf7cA83D837F9d04c59f41a92c1dC71C7d).call{value: balanceOne}("");
+        ( bool transferTwo, ) = payable(0x7ceB3cAf7cA83D837F9d04c59f41a92c1dC71C7d).call{value: balanceTwo}("");
+        require(transferOne && transferTwo, "Transfer failed.");
+    }
+
+    // OpenSea Enforcer functions
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override onlyAllowedOperator {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
